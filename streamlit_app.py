f37d4314c22f733b76c21a16931d99a441ca38da
hidevscommunity <130182096+hidevscommunity@users.noreply.github.com>
2023-10-05 15:23:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,22 +20,11 @@ st.markdown(
         Click on the **Generate Exercise** button to get an adapted exercise tailored to challenge students according to their skill level.*
 """
 )
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
+# Add custom CSS to hide the GitHub icon
 hide_github_icon = """
-
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+#GithubIcon {
+  visibility: hidden;
+}
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
